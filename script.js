8f96e760fdc5401b2097c0721ba68884c10ac184
Tague Zachary <taguezz@gmail.com>
2023-12-27 19:52:30 -0800

    add random element picker and element combiner

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //console.log('Hello World.')
 
-const commandNegative = ["Don't be like", 'My momma said don\'t trust']
-const commandPositive = ["Be like", 'Take it from', 'You want a champion? Look at' ]
-const thingNegative = ["Chuck", "the '98 Nuggets", 'rich-people food', 'me' ]
-const thingPositive = ["My momma", '11 time NBA Champion, Phil Jackson', 'me']
-const advice= ["Quit playin with me", "Be a man", "Just say sorry", "Family comes first", "I am Superman", "Love is a verb, not a noun.", "I'd punch em straight in the mouth" ]
+const commandNegative = ["Don't be like", 'My momma said don\'t trust', 'Haters be like'];
+const commandPositive = ["Be like", 'Take it from', 'You want a champion? Look at' ];
+const thingNegative = ["Chuck", "the '98 Nuggets", 'rich-people food', 'me' ];
+const thingPositive = ["my momma", '11 time NBA Champion, Phil Jackson', 'me'];
+const advice = ["Quit playin with me", "Be a man", "Just say sorry", "Family comes first", "I am Superman", "Love is a verb, not a noun.", "I'd punch em straight in the mouth" ];
 
+const pickRandEl = arr => {
+    let randEl = arr[Math.floor(Math.random() * arr.length)];
+    //console.log(randEl);
+    return randEl;
+}
+
+console.log(`${pickRandEl(commandPositive)} ${pickRandEl(thingPositive)}: ${pickRandEl(advice)}`);
