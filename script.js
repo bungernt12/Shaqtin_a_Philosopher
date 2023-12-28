246d9ae636fb6396fe13901613bf4c848580f8a2
Tague Zachary <taguezz@gmail.com>
2023-12-27 19:17:25 -0800

    Add array of good and bad commands, things, and advice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello World.')
+//console.log('Hello World.')
+
+const commandNegative = ["Don't be like", 'My momma said don\'t trust']
+const commandPositive = ["Be like", 'Take it from', 'You want a champion? Look at' ]
+const thingNegative = ["Chuck", "the '98 Nuggets", 'rich-people food', 'me' ]
+const thingPositive = ["My momma", '11 time NBA Champion, Phil Jackson', 'me']
+const advice= ["Quit playin with me", "Be a man", "Just say sorry", "Family comes first", "I am Superman", "Love is a verb, not a noun.", "I'd punch em straight in the mouth" ]
+
